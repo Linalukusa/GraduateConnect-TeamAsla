bc0a0360d4cac49cf4c23840d4a22bd7381a1b76
Sipho <smlsip007@myuct.ac.za>
2019-07-26 00:23:04 +0200

    admin code

--- GraduateConnect/src/app/app.component.ts
+++ GraduateConnect/src/app/app.component.ts
@@ -11,7 +11,16 @@ import { AuthService } from './shared/services/auth.service';
 })
 export class AppComponent {
   title = 'angularfirebase-authentication';
-  currentUser: User;
+  public newToken =
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsInJvbGUiOiJ3cml0ZXIiLCJqdGkiOiIzMGM5YTA4ZC1mYzg2LTQ0NDQtYjE0OC04NmNmMjA3ODhmNmIiLCJpYXQiOjE1MzgxMzgzMzUsImV4cCI6MTUzODE0Mzc0NH0.Twq_iUdZiGWp4ZK3o1J5UAQMgsBRzprGVuEJogYEDcA';
+
+  ngOnInit() {
+    localStorage.setItem('token', this.newToken);
+  }
+
+  userNewToken() {
+    localStorage.setItem('token', this.newToken);
+  }
 
 
 }
